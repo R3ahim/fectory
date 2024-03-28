a36b77a8cc3d8c4e39bf4225e4fdef3a3bf8b91b
r3ahim <r9ahim@gmail.com>
2024-03-29 02:42:08 +0600

    add expance done

--- src/mother/screens/Porfolio.js
+++ src/mother/screens/Porfolio.js
@@ -1,32 +1,121 @@
-import { View, Platform, Text ,StyleSheet,Button, TouchableOpacity,ScrollView, KeyboardAvoidingView} from 'react-native'
+import { View, Platform, Text ,StyleSheet,Button,DatePickerAndroid, TouchableOpacity,ScrollView, KeyboardAvoidingView} from 'react-native'
 import React,{useState} from 'react'
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
 import { Separator } from '../../components'
 // import { ScrollView } from 'react-native-gesture-handler';
 import { TextInput } from 'react-native-paper';
 export default function Portfolio() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [jatayat,setJatayat]= useState("")
+  const [nasta, setNasta] = useState('');
+  const [officeEx,setOfficeEx] = useState('');
+  const [sellery,setSellery] = useState("");
+  const [boosting, setBoosting] = useState('');
+  const [cloth,setCloth] = useState('');
+  const [btnOthers,setBtnOthers] = useState('');
+  const [panjabi,setPanjabi] = useState("");
+  const [ambroida,setAmbroda] = useState("");
+  const [othersPanjabi,setOtherPanj]= useState('');
+  const [vibid,setVibid]= useState('') 
+  const [isTimePickerVisible, setTimePickerVisible] = useState(false);
+  const [selectedTime, setSelectedTime] = useState('');
 
-  const handleNameChange = (text) => {
-    setName(text);
+  const showTimePicker = () => {
+    setTimePickerVisible(true);
   };
 
-  const handleEmailChange = (text) => {
-    setEmail(text);
+  const hideTimePicker = () => {
+    setTimePickerVisible(false);
   };
 
-  const handleMessageChange = (text) => {
-    setMessage(text);
+  const handleTimeConfirm = (time) => {
+    setSelectedTime(time.toLocaleTimeString());
+    hideTimePicker();
+  };
+  const handleNasta = (text) => {
+    setNasta(text);
+  };
+  const handleJatayat =(text)=>{
+    setJatayat(text)
+  }
+  const handleOffice = (text) => {
+    setOfficeEx(text);
+  };
+  const handleSellery = (text) => {
+    setSellery(text);
+  };
+  const handleBooting = (text) => {
+    setBoosting(text);
+  };
+  const handleCloth = (text) => {
+    setCloth(text);
+  };
+  const hanldeButtonOthers = (text) => {
+    setBtnOthers(text);
+  };
+  const hanldePanjabi = (text) => {
+    setPanjabi(text);
+  };
+  const hanldeAmboida = (text) => {
+    setAmbroda(text);
+  };
+  const handleOtherPan = (text) => {
+    setOtherPanj(text);
+  };
+  const handleVivid = (text) => {
+    setVibid(text);
   };
 
-  const handleSubmit = () => {
+
+  const handleSubmit = async() => {
     // Do something with the form data, e.g., send it to a server
-    console.log('Form submitted:', { name, email, message });
     // Clear the form fields
-    setName('');
-    setEmail('');
-    setMessage('');
+    const data = {
+      date:selectedTime,
+      jatayat:jatayat,
+      nasta:nasta,
+      sellery:sellery,
+      boosting:boosting,
+      btnOthers:btnOthers,
+      cloth:cloth,
+      panjabi:panjabi,
+      ambroida:ambroida,
+      othersPanjabi:othersPanjabi,
+      vibid:vibid
+    }
+    try {
+      const response = await fetch('https://deltaorders.onrender.com/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log('Order submitted successfully!');
+        // Handle success, maybe show a success message to the user
+      } else {
+        console.log('Failed to submit order');
+        // Handle error, maybe show an error message to the user
+      }
+    } catch (error) {
+      console.error('Error submitting order:', error);
+      // Handle network errors
+    }
+    console.log(data)
+  
+    setNasta('');
+    setOfficeEx('');
+    setSellery('');
+    setBoosting('');
+    setCloth('');
+    setBtnOthers('');
+    setPanjabi('');
+    setAmbroda('');
+    setOtherPanj('');
+    setVibid('');
   };
 
   return (
@@ -37,106 +126,127 @@ export default function Portfolio() {
     <ScrollView contentContainerStyle={styles.scrollContainer}>
     <View style={styles.inner}>
       <Text style={{color:'green',fontSize:20,margin:20,textAlign:'center',marginTop:50}}>Daily Expense</Text>
+      <View style={{ marginBottom: 5 }}>
+      <Button title={selectedTime?selectedTime:'Sellect Time'} onPress={showTimePicker} />
+      <DateTimePickerModal
+        isVisible={isTimePickerVisible}
+        mode="time"
+        onConfirm={handleTimeConfirm}
+        onCancel={hideTimePicker}
+      />
+    </View>
       <TextInput
        mode="outlined"
-       label="Outlined input"
+       label="Nasta"
        placeholder="Type something"
        right={<TextInput.Affix text="sells" />}
        style={styles.input}
-        value={email}
-        onChangeText={handleEmailChange}
+        value={nasta}
+        onChangeText={handleNasta}
         keyboardType="email-address"
       />
         <TextInput
        mode="outlined"
-       label="Outlined input"
+       label="Jatayat"
        placeholder="Type something"
        right={<TextInput.Affix text="sells" />}
        style={styles.input}
-        value={email}
-        onChangeText={handleEmailChange}
+        value={jatayat}
+        onChangeText={handleJatayat}
         keyboardType="email-address"
       />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={handleEmailChange}
+<TextInput
+       mode="outlined"
+       label="Office Expance"
+       placeholder="Type something"
+       right={<TextInput.Affix text="sells" />}
+       style={styles.input}
+        value={officeEx}
+        onChangeText={handleOffice}
         keyboardType="email-address"
       />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={handleEmailChange}
+<TextInput
+       mode="outlined"
+       label="Sellery"
+       placeholder="Type something"
+       right={<TextInput.Affix text="sells" />}
+       style={styles.input}
+        value={sellery}
+        onChangeText={handleSellery}
         keyboardType="email-address"
       />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={handleEmailChange}
+<TextInput
+       mode="outlined"
+       label="Boosting Expnace"
+       placeholder="Type something"
+       right={<TextInput.Affix text="sells" />}
+       style={styles.input}
+        value={boosting}
+        onChangeText={handleBooting}
         keyboardType="email-address"
       />
- 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={handleEmailChange}
+ <TextInput
+       mode="outlined"
+       label="Cloth Buy"
+       placeholder="Type something"
+       right={<TextInput.Affix text="sells" />}
+       style={styles.input}
+        value={cloth}
+        onChangeText={handleCloth}
         keyboardType="email-address"
       />
-
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={handleEmailChange}
-        keyboardType="email-address"
-      />
+       <TextInput
+      mode="outlined"
+      label="Button + Others"
+      placeholder="Type something"
+      right={<TextInput.Affix text="sells" />}
+      style={styles.input}
+       value={btnOthers}
+       onChangeText={hanldeButtonOthers}
+       keyboardType="email-address"
+     />
  
  <TextInput
        mode="outlined"
-       label="Outlined input"
+       label="Panjabi Seleni"
        placeholder="Type something"
        right={<TextInput.Affix text="sells" />}
        style={styles.input}
-        value={email}
-        onChangeText={handleEmailChange}
+        value={panjabi}
+        onChangeText={hanldePanjabi}
         keyboardType="email-address"
       />
   <TextInput
        mode="outlined"
-       label="Outlined input"
+       label="Ambroida Ri"
        placeholder="Type something"
        right={<TextInput.Affix text="sells" />}
        style={styles.input}
-        value={email}
-        onChangeText={handleEmailChange}
+        value={ambroida}
+        onChangeText={hanldeAmboida}
         keyboardType="email-address"
       />
- 
-      <TextInput
-        style={styles.input}
-        label={"Out Link INput"}
-        mode='outlined'
-        placeholder="Out Link INput"
-        value={email}
-        onChangeText={handleEmailChange}
+  <TextInput
+       mode="outlined"
+       label="Ohters Panjabi"
+       placeholder="Type something"
+       right={<TextInput.Affix text="sells" />}
+       style={styles.input}
+        value={othersPanjabi}
+        onChangeText={handleOtherPan}
         keyboardType="email-address"
       />
-
       <TextInput
        mode="outlined"
-       label="Outlined input"
+       label="Vibid"
        placeholder="Type something"
        right={<TextInput.Affix text="sells" />}
        style={styles.input}
-        value={email}
-        onChangeText={handleEmailChange}
+        value={vibid}
+        onChangeText={handleVivid}
         keyboardType="email-address"
       />
  
--- src/mother/screens/Prices.js
+++ src/mother/screens/Prices.js
@@ -1,14 +1,55 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet, ScrollView ,} from 'react-native';
 import React from 'react'
+import { DataTable } from 'react-native-paper';
 
 export default function Prices() {
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>Prices</Text>
-      <TextInput
-        style={{ fontSize: 17 }}
-        placeholder="Search for Restaurant item or more"
-      />
+    <View style={{flex: 1,marginTop:60}}>
+
+      <View>
+      <ScrollView horizontal>
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Name</DataTable.Title>
+        <DataTable.Title numeric>Age</DataTable.Title>
+        <DataTable.Title numeric>Weight</DataTable.Title>
+        <DataTable.Title>Name</DataTable.Title>
+        <DataTable.Title numeric>Age</DataTable.Title>
+        <DataTable.Title numeric>Weight</DataTable.Title>
+        <DataTable.Title>Name</DataTable.Title>
+        <DataTable.Title numeric>Age</DataTable.Title>
+        <DataTable.Title numeric>Weight</DataTable.Title>
+      </DataTable.Header>
+
+      <DataTable.Row>
+        <DataTable.Cell>John</DataTable.Cell>
+        <DataTable.Cell numeric>35</DataTable.Cell>
+        <DataTable.Cell numeric>180</DataTable.Cell>
+        <DataTable.Cell>John</DataTable.Cell>
+        <DataTable.Cell numeric>35</DataTable.Cell>
+        <DataTable.Cell numeric>180</DataTable.Cell>
+        <DataTable.Cell>John</DataTable.Cell>
+        <DataTable.Cell numeric>35</DataTable.Cell>
+        <DataTable.Cell numeric>180</DataTable.Cell>
+        <DataTable.Cell>John</DataTable.Cell>
+        <DataTable.Cell numeric>35</DataTable.Cell>
+        <DataTable.Cell numeric>180</DataTable.Cell>
+        <DataTable.Cell>John</DataTable.Cell>
+        <DataTable.Cell numeric>35</DataTable.Cell>
+        <DataTable.Cell numeric>180</DataTable.Cell>
+        <DataTable.Cell>John</DataTable.Cell>
+        <DataTable.Cell numeric>35</DataTable.Cell>
+        <DataTable.Cell numeric>180</DataTable.Cell>
+      </DataTable.Row>
+
+      <DataTable.Row>
+        <DataTable.Cell>Jane</DataTable.Cell>
+        <DataTable.Cell numeric>28</DataTable.Cell>
+        <DataTable.Cell numeric>150</DataTable.Cell>
+      </DataTable.Row>
+    </DataTable>
+    </ScrollView>
+  </View>
     </View>
   )
 }
